ca5a9cd957a4229d4a36f3b773161c8ff79d3dbf
beatrizosuna <btrzosuna@gmail.com>
2024-10-26 10:34:16 -0700

    Se agrega pantalla con nuevo menu de todos los programas

---
+++ app/src/main/java/com/example/primerapractica/menuActivity.kt
@@ -0,0 +1,50 @@
+package com.example.primerapractica
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable.Creator
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class menuActivity : AppCompatActivity() {
+    private lateinit var crvHola: CardView
+    private lateinit var crvImc : CardView
+    private lateinit var crvGrados : CardView
+    private lateinit var crvMoneda : CardView
+    private lateinit var crvCotizacion : CardView
+    private lateinit var crvSalir : CardView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun iniciarComponentes(){
+        crvHola = findViewById(R.id.crvHola) as CardView
+        crvImc = findViewById(R.id.crvImc) as CardView
+        crvGrados = findViewById(R.id.crvConversion) as CardView
+        crvMoneda = findViewById(R.id.crvMoneda) as CardView
+        crvCotizacion = findViewById(R.id.crvCotizacion) as CardView
+        crvSalir = findViewById(R.id.crvSalir) as CardView
+
+    }
+
+    fun eventosClic(){
+        crvHola.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        })
+    }
+}
